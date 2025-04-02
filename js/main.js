8b060ab5637c1e73309824113202722d488385a7
mariammahmoud5 <mariammahmoudelsayed123@gmail.com>
2025-04-02 16:20:57 +0200

    navs, slider and new products cards

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var myCarousel = new bootstrap.Carousel(document.querySelector('#carouselExample'), {
-    interval: 3000, // الانتقال كل 3 ثوانٍ
-    pause: false,   // لا يتوقف عند مرور الماوس
-    wrap: true      // يستمر بالدوران بعد آخر صورة
+    interval: 3000,
+    pause: false, 
+    wrap: true     
 });
