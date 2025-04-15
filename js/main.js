72b8f4f89ef2ac20f7b3fbff5bc63ead8d56f351
mariam <mariammahmoudelsayed123@gmail.com>
2025-04-16 01:06:36 +0200

    creating the Footer

---
+++ js/main.js
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+var myCarousel = new bootstrap.Carousel(document.querySelector('#carouselExample'), {
+    interval: 3000,
+    pause: false, 
+    wrap: true     
+});
+=======
+var table = $('#example').DataTable();
+ 
+table.search( 'Fiona' );
+table.draw();
+>>>>>>> 02cd4b28ef065ef6c17e1c4d0133f56ae2931dd0
